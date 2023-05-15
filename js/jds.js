a80edd898c65c7106022b339fbdaa3ad11195c8b
minayouns <133612507+minayouns@users.noreply.github.com>
2023-05-15 09:33:09 +0900

    Add files via upload

---
+++ js/jds.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        auto: true,
+        pause: 3000,
+    });
+  });
+  $(document).ready(function(){
+    $('.best_pd').bxSlider({
+        auto: true,
+        pause: 3000,
+        controls: false,
+        minSlides: 3,
+        maxSlides: 3,
+        moveSlides: 1,
+        slideWidth: 380,
+        slideMargin: 100,
+    });
+  });
